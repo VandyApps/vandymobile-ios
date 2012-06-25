f9f847096836b8d866b71534d13259032679eec0
Scott Andrus <scott.andrus@vanderbilt.edu>
2012-06-24 20:54:50 -0500

    Synthesized root object in app delegate to resolve issue with older compilers

--- VandyMobile/VMAppDelegate.m
+++ VandyMobile/VMAppDelegate.m
@@ -14,6 +14,7 @@
 @implementation VMAppDelegate
 
 @synthesize window = _window;
+@synthesize root = _root;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
