e41fd95c661c5c378e2c66b789a35ff90a9884cc
gg67 <graham.gaylor@chaione.com>
2012-07-02 01:18:34 -0600

    Define Meeting Dictionary keys

--- VandyMobile/Meeting.m
+++ VandyMobile/Meeting.m
@@ -8,6 +8,16 @@
 
 #import "Meeting.h"
 
+#define DAY_KEY @"day"
+#define DATE_KEY @"date"
+#define TIME_KEY @"date"
+#define HAS_FOOD_KEY @"food"
+#define HAS_SPEAKER_KEY @"speaker"
+#define SPEAKER_KEY @"speaker_name"
+#define TOPIC_KEY @"topic"
+#define DESCRIPTION_KEY @"description"
+
+
 @implementation Meeting
 
 @synthesize day = _day;
@@ -24,13 +34,13 @@
 - (id)initWithDictionary:(NSDictionary *)dictionary {
 	self = [super init];
 	if (self) {
-		self.day = [dictionary objectForKey:@"day"];
-		self.date = [self convertDateToFormattedStringWithJSONString:[dictionary objectForKey:@"date"]];
-		self.time = [self convertTimeToStringWithJSONString:[dictionary objectForKey:@"date"]];
-		self.hasFood = [NSNumber numberWithInt:[[dictionary objectForKey:@"food"] boolValue]];
-		self.hasSpeaker = [NSNumber numberWithInt:[[dictionary objectForKey:@"speaker"]boolValue]];
-		self.speakerName = [dictionary objectForKey:@"speaker_name"];
-		self.topic = [dictionary objectForKey:@"topic"];
+		self.day = [dictionary objectForKey:DAY_KEY];
+		self.date = [self convertDateToFormattedStringWithJSONString:[dictionary objectForKey:DATE_KEY]];
+		self.time = [self convertTimeToStringWithJSONString:[dictionary objectForKey:TIME_KEY]];
+		self.hasFood = [NSNumber numberWithInt:[[dictionary objectForKey:HAS_FOOD_KEY] boolValue]];
+		self.hasSpeaker = [NSNumber numberWithInt:[[dictionary objectForKey:HAS_SPEAKER_KEY]boolValue]];
+		self.speakerName = [dictionary objectForKey:SPEAKER_KEY];
+		self.topic = [dictionary objectForKey:TOPIC_KEY];
         self.dateUnformatted = [dictionary objectForKey:@"date"];
         
         // Protection on coordinate.
@@ -38,13 +48,26 @@
             self.loc = CLLocationCoordinate2DMake([[dictionary objectForKey:@"xcoordinate"] doubleValue], [[dictionary objectForKey:@"ycoordinate"] doubleValue]);
         }
         
-        self.description = [dictionary objectForKey:@"description"];
+        self.description = [dictionary objectForKey:DESCRIPTION_KEY];
 
 	}
 	return self;
 }
 
+- (NSDictionary *)meetingDictionary {
+	NSMutableDictionary *meetingDict;
+	[meetingDict setObject:self.day forKey:DAY_KEY];
+	[meetingDict setObject:self.speakerName forKey:SPEAKER_KEY];
+	[meetingDict setObject:self.topic forKey:TOPIC_KEY];
+	[meetingDict setObject:self.description forKey:DESCRIPTION_KEY];
+	
+	return [meetingDict copy];
+}
+
 - (NSString *)convertDateToFormattedStringWithJSONString:(NSString *)dateString {
+	if (!dateString) {
+		return @"N/a";
+	}
 	//Converts JSON date to a formatted date string
 	NSDateFormatter* dateFormatter = [[NSDateFormatter alloc] init];
 	[dateFormatter setTimeZone:[NSTimeZone timeZoneWithName:@"UTC"]];
@@ -64,6 +87,9 @@
 }
 
 - (NSString *)convertTimeToStringWithJSONString:(NSString *)dateString {
+	if (!dateString) {
+		return @"N/a";
+	}
 	//Converts JSON date to a time string
 	NSDateFormatter *timeFormatter = [[NSDateFormatter alloc]init];
 	[timeFormatter setTimeZone:[NSTimeZone timeZoneWithName:@"UTC"]];
