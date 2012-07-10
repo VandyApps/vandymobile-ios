176bb486623021769c404fcae90edc8260f72f31
gg67 <graham.gaylor@chaione.com>
2012-07-10 12:05:52 -0500

    Added UserAPIClient

---
+++ VandyMobile/UserAPIClient.h
@@ -0,0 +1,13 @@
+//
+//  UserAPIClient.h
+//  VandyMobile
+//
+//  Created by Graham Gaylor on 7/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AFHTTPClient.h"
+
+@interface UserAPIClient : AFHTTPClient
+
+@end
---
+++ VandyMobile/UserAPIClient.m
@@ -0,0 +1,13 @@
+//
+//  UserAPIClient.m
+//  VandyMobile
+//
+//  Created by Graham Gaylor on 7/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "UserAPIClient.h"
+
+@implementation UserAPIClient
+
+@end
